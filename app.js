b1d559db202dfb0ce2ae29e874ca098df8cf1790
Richard Lee <rblee88@gmail.com>
2015-07-13 21:59:08 -0500

    Initial Flappy Bird

---
+++ app.js
@@ -0,0 +1,62 @@
+var game = new Phaser.Game(400, 490, Phaser.AUTO, '')
+
+function Main() {};
+
+Main.prototype = {
+  preload: function(){
+    game.stage.backgroundColor = '#71c5cf';
+    game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
+  },
+  create: function(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.bird = this.game.add.sprite(100, 245, 'bird');
+    this.pipes = game.add.group();
+    this.pipes.enableBody = true;
+    this.pipes.createMultiple(20, 'pipe');
+
+    game.physics.arcade.enable(this.bird);
+    this.bird.body.gravity.y = 1000;
+
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
+  },
+  update: function(){
+    if(this.bird.inWorld == false){
+      this.restartGame();
+    }
+    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+  },
+  jump: function(){
+    this.bird.body.velocity.y = -350;
+  },
+  restartGame: function(){
+    game.state.start('main');
+  },
+  addOnePipe: function(x, y){
+    var pipe = this.pipes.getFirstDead();
+    pipe.reset(x, y);
+    pipe.body.velocity.x = -200;
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+  addRowOfPipes: function(){
+    var hole = Math.floor(Math.random() * 4) + 1;
+    this.score += 1;
+    this.labelScore.text = this.score;
+
+    for(var i = 0; i < 8; i++){
+      if(i != hole && i != hole+1 && i != hole+2){
+        this.addOnePipe(400, i * 60 + 10)
+      }
+    }
+  }
+};
+
+game.state.add('main', Main);
+game.state.start('main')
