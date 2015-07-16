f387e3e9a6a1b593abdc5046bd3a415306e4b83e
Richard Lee <rblee88@gmail.com>
2015-07-15 20:47:25 -0500

    Add sound and death animation

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ Main.prototype = {
     game.stage.backgroundColor = '#71c5cf';
     game.load.image('bird', 'assets/bird.png');
     game.load.image('pipe', 'assets/pipe.png');
+    game.load.audio('jump', 'assets/jump.wav');
   },
   create: function(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.jumpSound = game.add.audio('jump');
     this.bird = this.game.add.sprite(100, 245, 'bird');
+    this.bird.anchor.setTo(-0.2, 0.5);
     this.pipes = game.add.group();
     this.pipes.enableBody = true;
     this.pipes.createMultiple(20, 'pipe');
@@ -27,12 +30,22 @@ Main.prototype = {
     this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
   },
   update: function(){
+    if(this.bird.angle < 20){
+      this.bird.angle += 1;
+    }
     if(this.bird.inWorld == false){
       this.restartGame();
     }
-    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+    game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
   },
   jump: function(){
+    if(this.bird.alive == false){
+      return;
+    }
+    this.jumpSound.play();
+    var animation = game.add.tween(this.bird);
+    animation.to({angle: -20}, 100);
+    animation.start();
     this.bird.body.velocity.y = -350;
   },
   restartGame: function(){
@@ -55,6 +68,16 @@ Main.prototype = {
         this.addOnePipe(400, i * 60 + 10)
       }
     }
+  },
+  hitPipe: function(){
+    if(this.bird.alive == false){
+      return;
+    }
+    this.bird.alive = false;
+    game.time.events.remove(this.timer);
+    this.pipes.forEachAlive(function(p){
+      p.body.velocity.x = 0;
+    }, this)
   }
 };
 
